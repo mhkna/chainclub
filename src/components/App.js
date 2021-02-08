60db08aa33ea3a64cd06e263518cbb5cc8b2799c
mhkna <>
2021-02-08 17:45:46 -0500

    convert img for ipfs

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,28 @@ import React, { useState, useEffect, useRef } from 'react';
 import Web3 from 'web3';
 import ChainClub from '../abis/ChainClub.json'
 import Main from './Main'
+// import fs from 'promise-fs'
+
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 
 function App() {
   const [loading, setLoading] = useState(true)
   const [account, setAccount] = useState('')
-  const [chainclub, setChainclub] = useState('')
+  const [chainclub, setChainclub] = useState({})
   const [postArr, setPostArr] = useState([])
-  //// TEMP
+
   const renderCount = useRef(1)
 
   useEffect(() => {
-    loadData()
+    loadInitial()
   }, [])
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1
   })
 
-  async function loadData() {
+  async function loadInitial() {
     const web3 = await new Web3(Web3.givenProvider || 'http://localhost:7545')
     const accounts = await web3.eth.getAccounts()
     setAccount(accounts[0])
@@ -27,18 +31,35 @@ function App() {
     const chainclub = await web3.eth.Contract(ChainClub.abi, ChainClub.networks[networkId].address)
     setChainclub(chainclub)
     const postCount = await chainclub.methods.postCount().call()
-    for (let i = 1; i <= postCount; i++) {
+    for (let i = 1; i <= postCount.toNumber(); i++) {
       const post = await chainclub.methods.posts(i).call()
       setPostArr(prevPostArr => [...prevPostArr, post])
     }
     setLoading(false)
   }
 
+  function addPost(buffer, text) {
+    ipfs.add(buffer, (err, result) => {
+      if(err) {
+        return console.error(err)
+      }
+      chainclub.methods.addPost(result[0].hash, text)
+      .send({ from: account })
+      .on('transactionHash', (hash) => {
+        setLoading(false)
+      })
+    })
+  }
+
   return (
     <div>
       { loading
         ? <div>Loading...</div>
-        : <Main account={account}/>
+        : <Main
+            account={account}
+            addPost={addPost}
+            postArr={postArr}
+          />
       }
       <div>Rendered {renderCount.current} times.</div>
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
 
 function Main(props) {
-  const [value, setValue] = useState('')
+  const [textValue, setTextValue] = useState('')
+  const [imgValue, setImgValue] = useState([])
 
-  async function handleChange(e) {
-    setValue(e.target.value)
+  function handleTextChange(e) {
+    setTextValue(e.target.value)
   }
 
-  function handleSubmit(e) {
+  async function convertPost(e) {
+    const imgObj = e.target.files[0];
+    const reader = new FileReader()
+    await reader.readAsArrayBuffer(imgObj)
+    reader.onloadend = () => {
+      setImgValue(Buffer(reader.result))
+    }
+  }
+
+  async function handleSubmit(e) {
     e.preventDefault()
-    props.addPost(value)
+    props.addPost(imgValue, textValue)
+  }
+
+  function postList() {
+    const listItems = props.postArr.map((post) =>
+      <li key={post.id.toNumber()}>
+        <div>{post.text}</div>
+        <img src={`https://ipfs.infura.io/ipfs/${post.hash}`}/>
+      </li>
+    )
+    return (
+      <ul>
+        {listItems}
+      </ul>
+    )
   }
 
   return (
@@ -17,21 +41,22 @@ function Main(props) {
       <div>Account #: {props.account}</div>
 
       <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={value} onChange={handleChange} />
-        </label>
+        <div>
+          Text:
+          <input type="text" value={textValue} onChange={handleTextChange} />
+        </div>
+        <div>
+          Image:
+          <input type="file" accept=".jpg, .png" onChange={convertPost} />
+        </div>
         <input type="submit" value="Submit" />
       </form>
+      <div>
+        {postList()}
+      </div>
     </div>
   );
 
 }
 
 export default Main;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     textAlign: 'center'
-//   }
-// })
